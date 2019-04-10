1fd85b26fb9463bfc9e068a843f2c250d5afdddf
Gabriel <gabriel@kensho.com>
2019-04-10 04:25:47 +0000

    output entities to json and WikidataEntity type

--- qwikidata/utils.py
+++ qwikidata/utils.py
@@ -2,7 +2,10 @@
 """qwikidata utilities."""
 
 import itertools
+import json
 from typing import Iterable, Iterator, Tuple
+
+from qwikidata.entity import WikidataEntity
 
 
 def pairwise(iterable: Iterable) -> Iterator[Tuple]:
@@ -10,3 +13,14 @@ def pairwise(iterable: Iterable) -> Iterator[Tuple]:
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+def dump_entities_to_json(entities: Iterable[WikidataEntity], out_fname: str) -> None:
+    with open(out_fname, "w") as fp:
+        fp.write("[\n")
+        for ent_lo, ent_hi in pairwise(entities):
+            ent_str = json.dumps(ent_lo._entity_dict)
+            fp.write("{},\n".format(ent_str))
+        ent_str = json.dumps(ent_hi._entity_dict)
+        fp.write("{}".format(ent_str))
+        fp.write("\n]")
